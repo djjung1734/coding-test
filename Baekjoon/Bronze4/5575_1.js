3441fbd59da52ac9ba8ef84e753f1f573e57fcd3
djjung <ekwls8460@gmail.com>
2024-06-14 23:43:35 +0900

    24.06.16

---
+++ Baekjoon/Bronze4/5575_1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+let input = fs.readFileSync(filePath).toString().split('\n').map(f=>f.split(' ').map(Number));
+
+for(let i=0;i<input.length;i++){
+    let time1 = input[i][0]*60*60+input[i][1]*60+input[i][2];
+    let time2 = input[i][3]*60*60+input[i][4]*60+input[i][5];
+
+    let s = (time2-time1)%60;
+    let m = parseInt((time2-time1)/60%60);
+    let h = parseInt((time2-time1)/60/60);
+    
+    console.log(h,m,s);
+}
