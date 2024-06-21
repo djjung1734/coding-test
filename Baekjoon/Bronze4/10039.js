83c4be819452c1b2e6f1c481edafcf0ded906856
djjung <ekwls8460@gmail.com>
2024-06-21 23:05:11 +0900

    10039 수정

--- Baekjoon/Bronze4/10039.js
+++ Baekjoon/Bronze4/10039.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
-let input = fs.readFileSync(filePath).toString().split('\n').map(Number);
+let input = fs.readFileSync(filePath).toString().split('\n');
 
-let output = 0;
-for(let i=0;i<input.length;i++){
-    if(input[i]< 40){
-        input[i] = 40;
-    }
-    output += input[i];
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+  if (input[i] < 40) {
+    input[i] = '40';
+  }
+  sum += Number(input[i]);
 }
 
-console.log(output/5);
-
+let result = Math.floor(sum / 5);
+console.log(result);
