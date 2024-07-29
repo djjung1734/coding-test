347038c6f66246b8bd0f5d245d52e1a892f07b09
djjung <ekwls8460@gmail.com>
2024-07-30 00:06:10 +0900

    24.07.30

--- Programmers/lv.2/JadenCase_문자열_만들기.js
+++ Programmers/lv.2/JadenCase_문자열_만들기.js
@@ -1,10 +1,18 @@
 function solution(s) {
   s = s.split(" ");
-  s = s.map((value) =>
-    value.split("").map((v, i) => {
+  s = s.map((value) => {
+    value = value.split("").map((v, i) => {
       if (i === 0 && isNaN(v)) {
-        console.log(v);
+        v = v.toUpperCase();
+      } else if (isNaN(v)) {
+        v = v.toLowerCase();
       }
-    })
-  );
+      return v;
+    });
+    return value.join("");
+  });
+  return s.join(" ");
 }
+
+console.log(solution("3people unFollowed me"));
+console.log(solution("for the last week"));
