d7efa7309b5bb311116fd34f6caef0506362d74f
djjung <ekwls8460@gmail.com>
2024-08-04 15:34:37 +0900

    24.08.04
    - 효율성 테스트 완료

--- Programmers/lv.2/짝지어_제거하기.js
+++ Programmers/lv.2/짝지어_제거하기.js
@@ -1,12 +1,25 @@
+// function solution(s) {
+//   s = s.split("");
+//   for (let i = 0; i < s.length - 1; i++) {
+//     if (s[i] === s[i + 1]) {
+//       s.splice(i, 2);
+//       i -= 2;
+//     }
+//   }
+//   return s.length === 0 ? 1 : 0;
+// }
+
 function solution(s) {
-  s = s.split("");
-  for (let i = 0; i < s.length - 1; i++) {
-    if (s[i] === s[i + 1]) {
-      s.splice(i, 2);
-      i -= 2;
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack.length > 0 && stack[stack.length - 1] === s[i]) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
     }
   }
-  return s.length === 0 ? 1 : 0;
+  return stack.length === 0 ? 1 : 0;
 }
 
 console.log(solution("baabaa"));
