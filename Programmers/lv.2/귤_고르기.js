8dfe91e9e94db08373d38851f58ac2e39bce489b
djjung <ekwls8460@gmail.com>
2024-08-10 17:31:47 +0900

    24.08.10

--- Programmers/lv.2/귤_고르기.js
+++ Programmers/lv.2/귤_고르기.js
@@ -1,24 +1,41 @@
+// function solution(k, tangerine) {
+//   const count = [];
+//   let sum = 0;
+//   let answer = 0;
+
+//   let filtered = tangerine.filter((v, i) => tangerine.indexOf(v) === i);
+//   filtered.map((value) => {
+//     let cnt = 0;
+//     tangerine.map((v) => {
+//       if (value === v) cnt++;
+//     });
+//     count.push(cnt);
+//   });
+//   count
+//     .sort((a, b) => b - a)
+//     .map((v) => {
+//       if (sum < k) {
+//         sum += v;
+//         answer++;
+//       }
+//     });
+//   return answer;
+// }
+
 function solution(k, tangerine) {
-  const count = [];
-  let sum = 0;
+  const count = {};
   let answer = 0;
 
-  let filtered = tangerine.filter((v, i) => tangerine.indexOf(v) === i);
-  filtered.map((value) => {
-    let cnt = 0;
-    tangerine.map((v) => {
-      if (value === v) cnt++;
-    });
-    count.push(cnt);
-  });
-  count
-    .sort((a, b) => b - a)
-    .map((v) => {
-      if (sum < k) {
-        sum += v;
-        answer++;
-      }
-    });
+  tangerine.forEach((t) => (count[t] = (count[t] || 0) + 1));
+  const sorted = Object.values(count).sort((a, b) => b - a);
+  for (let i = 0; i < sorted.length; i++) {
+    answer++;
+    if (sorted[i] < k) {
+      k -= sorted[i];
+    } else {
+      break;
+    }
+  }
   return answer;
 }
 
