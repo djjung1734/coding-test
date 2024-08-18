dc85ffc13ca380efe8214a339b1737439774243b
djjung <ekwls8460@gmail.com>
2024-08-18 11:07:39 +0900

    24.08.18

--- Programmers/lv.2/할인_행사.js
+++ Programmers/lv.2/할인_행사.js
@@ -1,13 +1,61 @@
 function solution(want, number, discount) {
-  const total = number.reduce((a, b) => a + b);
+  let answer = 0;
 
-  for (let i = 0; i <= discount.length - total; i++) {
-    let shopping = discount.slice(i, i + total);
-    let obj = {};
+  for (let i = 0; i <= discount.length - 10; i++) {
+    let shopping = discount.slice(i, i + 10);
+    let cnt = [];
     for (let j = 0; j < want.length; j++) {
-      obj[want[j]] = number[j];
+      cnt[j] = 0;
+      shopping.map((value, idx) => {
+        if (value === want[j]) {
+          cnt[j]++;
+        }
+      });
+    }
+    if (JSON.stringify(number) === JSON.stringify(cnt)) {
+      answer++;
     }
   }
+  return answer;
 }
 
-//진행중
+console.log(
+  solution(
+    ["banana", "apple", "rice", "pork", "pot"],
+    [3, 2, 2, 2, 1],
+    [
+      "chicken",
+      "apple",
+      "apple",
+      "banana",
+      "rice",
+      "apple",
+      "pork",
+      "banana",
+      "pork",
+      "rice",
+      "pot",
+      "banana",
+      "apple",
+      "banana",
+    ]
+  )
+);
+console.log(
+  solution(
+    ["apple"],
+    [10],
+    [
+      "banana",
+      "banana",
+      "banana",
+      "banana",
+      "banana",
+      "banana",
+      "banana",
+      "banana",
+      "banana",
+      "banana",
+    ]
+  )
+);
