76abc57ebb8887a54a703e81a41361ad39196a8f
djjung <ekwls8460@gmail.com>
2024-08-22 23:48:52 +0900

    24.08.22

---
+++ Programmers/lv.2/캐시.js
@@ -0,0 +1,80 @@
+function solution(cacheSize, cities) {
+  let cache = [];
+  let time = 0;
+
+  cities.map((value) => {
+    value = value.toLowerCase();
+    if (cache.includes(value)) {
+      cache = cache.filter((v, i) => cache.indexOf(value) !== i);
+      cache.push(value);
+      time += 1;
+    } else {
+      cache.push(value);
+      if (cache.length > cacheSize) cache.shift();
+      time += 5;
+    }
+  });
+  return time;
+}
+
+console.log(
+  solution(3, [
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+    "NewYork",
+    "LA",
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+    "NewYork",
+    "LA",
+  ])
+);
+console.log(
+  solution(3, [
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+  ])
+);
+console.log(
+  solution(2, [
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+    "NewYork",
+    "LA",
+    "SanFrancisco",
+    "Seoul",
+    "Rome",
+    "Paris",
+    "Jeju",
+    "NewYork",
+    "Rome",
+  ])
+);
+console.log(
+  solution(5, [
+    "Jeju",
+    "Pangyo",
+    "Seoul",
+    "NewYork",
+    "LA",
+    "SanFrancisco",
+    "Seoul",
+    "Rome",
+    "Paris",
+    "Jeju",
+    "NewYork",
+    "Rome",
+  ])
+);
+console.log(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]));
+console.log(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]));
