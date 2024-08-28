5a9b9b266afe13a91f6bdfa5c85d03f966d24e5c
djjung <ekwls8460@gmail.com>
2024-08-28 18:25:43 +0900

    24.08.28

---
+++ Programmers/lv.2/전화번호_목록.js
@@ -0,0 +1,16 @@
+function solution(phone_book) {
+  phone_book.sort();
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (
+      phone_book[i] === phone_book[i + 1].substring(0, phone_book[i].length)
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(solution(["119", "97674223", "1195524421"]));
+console.log(solution(["123", "456", "789"]));
+console.log(solution(["12", "123", "1235", "567", "88"]));
