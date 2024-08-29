6ade7fb9a4e67acd578b2d23abfe65df6de9128c
djjung <ekwls8460@gmail.com>
2024-08-29 21:25:40 +0900

    24.08.29

--- Programmers/lv.2/뒤에_있는_큰_수_찾기.js
+++ Programmers/lv.2/뒤에_있는_큰_수_찾기.js
@@ -1,16 +1,29 @@
+// function solution(numbers) {
+//   const answer = [];
+
+//   for (let i = 0; i < numbers.length; i++) {
+//     let num = 0;
+//     for (let j = 0; j < numbers.length; j++) {
+//       if (i >= j) continue;
+//       if (numbers[i] < numbers[j]) {
+//         num = numbers[j];
+//         break;
+//       }
+//     }
+//     num === 0 ? answer.push(-1) : answer.push(num);
+//   }
+//   return answer;
+// }
+
 function solution(numbers) {
-  const answer = [];
+  const answer = Array(numbers.length).fill(-1);
+  const stack = [];
 
   for (let i = 0; i < numbers.length; i++) {
-    let num = 0;
-    for (let j = 0; j < numbers.length; j++) {
-      if (i >= j) continue;
-      if (numbers[i] < numbers[j]) {
-        num = numbers[j];
-        break;
-      }
+    while (stack.length && numbers[stack.at(-1)] < numbers[i]) {
+      answer[stack.pop()] = numbers[i];
     }
-    num === 0 ? answer.push(-1) : answer.push(num);
+    stack.push(i);
   }
   return answer;
 }
