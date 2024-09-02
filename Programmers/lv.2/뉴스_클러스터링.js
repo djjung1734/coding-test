72650abb5bb8ed43df0e904c9bec35cc04393751
djjung <ekwls8460@gmail.com>
2024-09-02 23:57:24 +0900

    24.09.02

---
+++ Programmers/lv.2/뉴스_클러스터링.js
@@ -0,0 +1,11 @@
+function solution(str1, str2) {
+  const arr1 = [];
+  const arr2 = [];
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  for (let i = 0; i < str1.length; i++) {}
+  console.log(arr1);
+}
+
+//진행중
