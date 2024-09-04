ff7e9b3b8ba619b20557e36f9acd1b95e4ba62db
djjung <ekwls8460@gmail.com>
2024-09-05 00:08:49 +0900

    24.09.05

--- Programmers/lv.2/뉴스_클러스터링.js
+++ Programmers/lv.2/뉴스_클러스터링.js
@@ -5,33 +5,31 @@ function solution(str1, str2) {
   str2 = str2.toLowerCase();
 
   for (let i = 0; i < str1.length - 1; i++) {
-    if (/^[a-z]{2}$/.test(str1[i] + str1[i + 1])) {
+    if (/^[a-z]{2}$/g.test(str1[i] + str1[i + 1])) {
       arr1.push(str1[i] + str1[i + 1]);
     }
   }
   for (let i = 0; i < str2.length - 1; i++) {
-    if (/^[a-z]{2}$/.test(str2[i] + str2[i + 1])) {
+    if (/^[a-z]{2}$/g.test(str2[i] + str2[i + 1])) {
       arr2.push(str2[i] + str2[i + 1]);
     }
   }
 
   let intersection = 0;
 
-  arr1.forEach((value, idx) => {
-    for (let i = 0; arr2.length; i++) {
-      if (value === arr2[i]) {
-        arr2.splice(i, 1);
+  arr1.forEach((v) => {
+    for (let j = 0; j < arr2.length; j++) {
+      if (v === arr2[j]) {
+        arr2.splice(j, 1);
         intersection++;
         break;
       }
     }
   });
 
-  console.log(arr1);
-  console.log(arr2);
-  // let union = arr1.length + arr2.length;
+  let union = arr1.length + arr2.length;
 
-  // return union !== 0 ? parseInt((intersection / union) * 65536) : 65536;
+  return union === 0 ? 65536 : parseInt((intersection / union) * 65536);
 }
 
 console.log(solution("FRANCE", "french"));
