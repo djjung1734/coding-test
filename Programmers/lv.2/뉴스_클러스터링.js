2476378798c49383cec6d833f3acb9e68d05a56f
djjung <ekwls8460@gmail.com>
2024-09-04 23:54:42 +0900

    24.09.04

--- Programmers/lv.2/뉴스_클러스터링.js
+++ Programmers/lv.2/뉴스_클러스터링.js
@@ -4,30 +4,38 @@ function solution(str1, str2) {
   str1 = str1.toLowerCase();
   str2 = str2.toLowerCase();
 
-  for (let i = 0; i < str1.length; i++) {
-    if (
-      /^[a-zA-Z]*$/.test(str1.slice(i, i + 2)) &&
-      str1.slice(i, i + 2).length === 2
-    ) {
-      arr1.push(str1.slice(i, i + 2));
+  for (let i = 0; i < str1.length - 1; i++) {
+    if (/^[a-z]{2}$/.test(str1[i] + str1[i + 1])) {
+      arr1.push(str1[i] + str1[i + 1]);
     }
   }
-  for (let i = 0; i < str2.length; i++) {
-    if (
-      /^[a-zA-Z]*$/.test(str2.slice(i, i + 2)) &&
-      str2.slice(i, i + 2).length === 2
-    ) {
-      arr2.push(str2.slice(i, i + 2));
+  for (let i = 0; i < str2.length - 1; i++) {
+    if (/^[a-z]{2}$/.test(str2[i] + str2[i + 1])) {
+      arr2.push(str2[i] + str2[i + 1]);
     }
   }
+
+  let intersection = 0;
+
+  arr1.forEach((value, idx) => {
+    for (let i = 0; arr2.length; i++) {
+      if (value === arr2[i]) {
+        arr2.splice(i, 1);
+        intersection++;
+        break;
+      }
+    }
+  });
+
   console.log(arr1);
   console.log(arr2);
-  console.log(arr1.filter((v) => arr2.includes(v)));
-  console.log([
-    ...arr1.filter((v) => arr2.includes(v)),
-    ...arr1.filter((v) => !arr2.includes(v)),
-  ]);
+  // let union = arr1.length + arr2.length;
+
+  // return union !== 0 ? parseInt((intersection / union) * 65536) : 65536;
 }
 
-//진행중
-//같은 값일 때 합집합이 제대로 나오지 않음
+console.log(solution("FRANCE", "french"));
+console.log(solution("handshake", "shake hands"));
+console.log(solution("aa1+aa2", "AAAA12"));
+console.log(solution("E=M*C^2", "e=m*c^2"));
+//시간 초과
