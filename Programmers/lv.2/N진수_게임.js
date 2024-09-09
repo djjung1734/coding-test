5527e37e15cc0850245f5985aa4a68b3b43792c4
djjung <ekwls8460@gmail.com>
2024-09-09 16:15:37 +0900

    24.09.09

--- Programmers/lv.2/N진수_게임.js
+++ Programmers/lv.2/N진수_게임.js
@@ -1,11 +1,22 @@
 function solution(n, t, m, p) {
   let number = "";
+  let arr = [];
 
   let i = 0;
   while (number.length <= (t - 1) * m + p) {
     number += i.toString(n);
     i++;
   }
-  console.log(number);
+  for (let j = 0; j < number.length; j++) {
+    if (j % m === p - 1) {
+      arr.push(number[j]);
+    }
+  }
+  arr = arr.slice(0, t);
+
+  return arr.join("").toUpperCase();
 }
-//진행중
+
+console.log(solution(2, 4, 2, 1));
+console.log(solution(16, 16, 2, 1));
+console.log(solution(16, 16, 2, 2));
