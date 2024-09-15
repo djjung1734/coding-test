55ee54defc6d6ec5d51f2371ee941dbb4cc38402
djjung <ekwls8460@gmail.com>
2024-09-15 16:23:23 +0900

    24.09.15

--- Programmers/lv.3/야근_지수.js
+++ Programmers/lv.3/야근_지수.js
@@ -2,17 +2,23 @@ function solution(n, works) {
   let answer = 0;
   works.sort((a, b) => b - a);
 
-  if (works.reduce((a, b) => a + b) > n) {
-    for (let i = 0; i < n; i++) {
-      works[works.indexOf(Math.max(...works))]--;
+  if (works.reduce((a, b) => a + b) <= n) return 0;
+
+  while (n !== 0) {
+    for (let i = 0; i < works.length; i++) {
+      if (works[i] >= works[0]) {
+        n--;
+        works[i]--;
+      }
+      if (n === 0) break;
     }
-    answer = works.map((v) => v ** 2).reduce((a, b) => a + b);
   }
+
+  answer = works.reduce((a, b) => a + b ** 2, 0);
+
   return answer;
 }
 
 console.log(solution([4, 3, 3], 4));
 console.log(solution([2, 1, 2], 1));
 console.log(solution([1, 1], 3));
-
-//효율성 통과 못함
